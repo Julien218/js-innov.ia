9f9b7995feb81f9806ed59edf07ae2e87d82f512
Julien Pagin <paginjulien@gmail.com>
2025-11-20 01:35:06 +0000

    File changes

--- src/components/music/CustomMusicRequestModal.jsx
+++ src/components/music/CustomMusicRequestModal.jsx
@@ -51,7 +51,7 @@ ${data.message}`
 
   return (
     <AnimatePresence>
-      <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+      <div className="fixed inset-0 z-[70] flex items-center justify-center p-4">
         {/* Backdrop */}
         <motion.div
           initial={{ opacity: 0 }}
