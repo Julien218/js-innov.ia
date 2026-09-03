b53630ebe1ffb140e1eb298d851b822b02d087e3
Js-Innov.IA <info@jsinnovia.com>
2026-09-03 12:39:15 +0200

    Compiler les coques Pixelium et Signelya séparément

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        signelya: fileURLToPath(new URL('./signelya.html', import.meta.url)),
+      },
+    },
+  },
   plugins: [
     react(),
   ]
